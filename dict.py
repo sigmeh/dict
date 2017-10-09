06ca8d8741a47edc5d19f0688fab8666751535fb
sigmeh <markh@tuta.io>
2017-10-10 00:20:01 +0200

    improve parsing and readability

--- dict.py
+++ dict.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 '''
-Simple python command-line tool for dictionary lookup from 'thefreedictionary.com'. 
+Simple python command-line tool for dictionary (word definition) lookup from 'thefreedictionary.com'. 
 Utilizes the requests module for data retrieval and 
 python BeautifulSoup parser with lxml library. 
+The word to look up is supplied as a command line argument. 
 
 Usage:
 	
@@ -11,7 +12,7 @@ Usage:
 Add function to .bashrc to run command from any directory; e.g., 
 
 	dict(){
-		python <path_to_file>/dict.py $@
+		python <path_to_folder>/dict.py $@
 	}
 	
 	then, 
@@ -23,6 +24,7 @@ Add function to .bashrc to run command from any directory; e.g.,
 from bs4 import BeautifulSoup as bs
 import requests
 import sys
+import re
 
 headers = {
 		'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:43.0) Gecko/20100101 Firefox/44.0',
@@ -37,17 +39,36 @@ def main():
 		return
 		
 	term = sys.argv[1] 
+	
+	print
+	print '|---------- dict.py ----------|'
+	print
+	print '  Looking up %s... ' % term.upper()
+	print
+	
 	url = 'http://www.thefreedictionary.com/%s' % term
 	
 	r = requests.get(url,headers=headers)
 	doc = r.text
-
+	
 	soup = bs(doc, 'lxml')
 	definition = soup.find(id='Definition')
 	
 	sections = definition.find_all('section')
-	for i,s in enumerate(sections):
-		print '%s.  %s' % (i, s.get_text())
+
+	for i,s in enumerate(sections):		
+
+		print '%s... %s' % ( str(i+1), re.search('(?P<initial>.+?)[(\d\.)]',s.get_text() ).group('initial') )
+		
+		entries = [x.strip() for x in re.findall('\d\.([^\d]*)',s.get_text())]
+		
+		if entries:
+			print '\n'.join('  %s. %s' %(str(j+1),t) for j,t in enumerate(entries) )
+		else:
+			print '   1. %s' % s.find(attrs={'class':'ds-list'}).get_text()
+			
+		print '------------------------'
+		
 
 if __name__ == '__main__':
 	main()
